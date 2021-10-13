a21169911a5e403da695341fd23f403b7dde2ec3
onlylosersleftalive <malaniko888@mail.ru>
2021-10-13 21:26:11 +0300

    fix problems with TypeErrow, add default image

--- src/Books.js
+++ src/Books.js
@@ -5,12 +5,16 @@ const Books = (props) => {
   console.log(props.booksInfo);
   return (
     <section className="results-section">
-      {props.booksInfo.map((book, index) => (
+      {props?.booksInfo?.map((book, index) => (
         <BookCard
           key={index}
-          image={book.volumeInfo.imageLinks.thumbnail}
-          title={book.volumeInfo.title}
-          author={book.volumeInfo.authors}
+          image={
+            book.volumeInfo.hasOwnProperty("imageLinks") === false
+              ? "https://vignette.wikia.nocookie.net/pandorahearts/images/a/ad/Not_available.jpg/revision/latest?cb=20141028171337"
+              : book.volumeInfo.imageLinks.thumbnail
+          }
+          title={book.volumeInfo?.title}
+          author={book.volumeInfo?.authors}
         />
       ))}
     </section>
