fcf3051d046ec552cf7bb71101175d7cd88baeec
onlylosersleftalive <malaniko888@mail.ru>
2021-10-14 17:46:52 +0300

    remove categories sorting from Filter component

--- src/Filter.js
+++ src/Filter.js
@@ -1,28 +1,18 @@
 import "./Filter.css";
 
-const Filter = () => {
+const Filter = (props) => {
+  const sortChangeHandler = (event) => {
+    props.onSortChange(event.target.value);
+  };
+
   return (
     <div className="filter-container">
-      <div className="categories-filter">
-        <label>Categories:</label>
-        <select>
-          <option value="All">All</option>
-          <option value="Art">Art</option>
-          <option value="Biography">Biography</option>
-          <option value="Computers">Computers</option>
-          <option value="History">History</option>
-          <option value="Medical">Medical</option>
-          <option value="Poetry">Poetry</option>
-        </select>
-      </div>
-      <div className="sort-filter">
-        <label>Sort by:</label>
-        <select>
-          <option value="relevence">Relevence</option>
-          <option value="newest">Newest</option>
-          <option value="liked">Liked</option>
-        </select>
-      </div>
+      <label>Sort by:</label>
+      <select value={props.selectedSort} onChange={sortChangeHandler}>
+        <option value="relevance">Relevance</option>
+        <option value="newest">Newest</option>
+        <option value="liked">Liked</option>
+      </select>
     </div>
   );
 };
