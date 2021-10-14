6353f0c530ebcbaac9b3292ca7d9659dfaf3cda6
onlylosersleftalive <malaniko888@mail.ru>
2021-10-14 17:38:58 +0300

    add like and delete buttons, adjust styles

--- src/BookCard.js
+++ src/BookCard.js
@@ -6,9 +6,23 @@ const BookCard = (props) => {
       <div className="image-container">
         <img src={props.image} alt="The book cover" />
       </div>
+      <div className="actions-container">
+        <button>
+          <i class="far fa-heart"></i>
+        </button>
+        <button>
+          <i class="fas fa-trash"></i>
+        </button>
+      </div>
       <div className="info-container">
         <h2>{props.title}</h2>
         <p>{props.author}</p>
+        <p>
+          {props.publishedDate !== "0000"
+            ? props.publishedDate.substr(0, 4)
+            : ""}
+        </p>
+        <p>{props.category}</p>
       </div>
     </div>
   );
